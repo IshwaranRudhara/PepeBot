6df3c7de18df9e409ee0a1ce7dbbcdf45b711409
Ishwaranrudhara <39009926+IshwaranRudhara@users.noreply.github.com>
2020-06-19 15:32:33 +0530

    Update PKarbon3.py

--- stdplugins/PKarbon3.py
+++ stdplugins/PKarbon3.py
@@ -59,7 +59,7 @@ async def carbon_api(e):
    await e.client.send_file(
          e.chat_id,
          file,
-         caption="Karbon3 by [@PhycoNinja13b](https://github.com/Phyco-Ninja/UniNinja)",
+         caption="Karbon3",
          force_document=True,
          reply_to=e.message.reply_to_msg_id,
          )
