e8487b8d4c2928f5773bf32500efc1c566e680ba
Ishwaranrudhara <39009926+IshwaranRudhara@users.noreply.github.com>
2020-06-19 15:38:16 +0530

    Update carbon1.py

--- stdplugins/carbon1.py
+++ stdplugins/carbon1.py
@@ -79,8 +79,8 @@ async def carbon_api(e):
     await e.client.send_file(
         e.chat_id,
         file,
-        caption="Made using [Carbon](https://carbon.now.sh/about/),\
-        \na project by [Dawn Labs](https://dawnlabs.io/)",
+        caption="Made using [Carbon](www.google.com),\
+        \na project by [Dawn Labs](www.google.com)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
